1a3866a1918f12a3efbafc25ad890138054908dc
ibpark02 <ibpark02@gmail.com>
2025-11-19 21:28:54 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,8 @@
-#!/bin/bash
-
-# 친구 변수 설정
 DOCKER_USERNAME="parkinbeom"
 IMAGE_NAME="ipark"
 TAG="22.04-cpu-auto"
 FULL_IMAGE_NAME="$DOCKER_USERNAME/$IMAGE_NAME:$TAG"
 
-# 1. 빌드
 echo "-------------------------------------"
 echo "Building Docker image: $FULL_IMAGE_NAME"
 docker build -t $FULL_IMAGE_NAME .
@@ -16,7 +12,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# 2. 로그인
 echo "-------------------------------------"
 echo "Logging in..."
 docker login
@@ -26,7 +21,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# 3. 푸시
 echo "-------------------------------------"
 echo "Pushing image..."
 docker push $FULL_IMAGE_NAME
@@ -36,7 +30,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# 4. [완전 짧아진 실행 명령어 안내]
 echo "-------------------------------------"
 echo " [배포 완료!]"
 echo " 이제 아래 명령어 한 줄이면 끝납니다."
